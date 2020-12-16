064304d6d5d9a7f96519c39a70e44e006c1fa2c6
Jerry Yong Wee Chong <yongwc@lionfib.com.my>
2020-12-16 18:25:56 +0800

    16/12/2020 end of work day

--- product1.php
+++ product1.php
@@ -63,9 +63,11 @@
 		
 
 	
-        <div class="container white">
+        <div class="container white text-center">
+        	<h3 class="text-center pt-5">HI-REV 9140 PREMIUM FULLY SYNTHETIC MOTOR OIL</h3>
           	<div class="row ">
-            	<div class="col-lg-3 col-md-4 col-sm-12">
+            	<div class="col-lg-5 col-md-6 col-sm-12">
+            		<!--
 	              	<div class="productmenu mb-4 p-5">
 	              		<h2>Products</h2>
 	                	<a href="#" class="productmenu-link">Motor Oil</a><br/>
@@ -73,8 +75,9 @@
 	                	<a href="#" class="productmenu-link">Industrial Lubricants</a><br/>
 	                	<a href="#" class="productmenu-link">Other Products</a>
 	              	</div>
+	              -->
             	</div>
-            	<div class="col-lg-9 col-md-8  col-sm-12">
+            	<div class="col-lg-7 col-md-6  col-sm-12">
               		<div class="productoverview mb-4 pt-3">
                 		<div class="row ">
 			            	<div class="col-md-6 col-sm-12 min-h-350 p-5">
