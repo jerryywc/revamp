2147640efb560be140c3eea901adf12525790201
Jerry Yong Wee Chong <yongwc@lionfib.com.my>
2020-12-16 15:12:10 +0800

    First commit

---
+++ index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Revamp</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" 
+			integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  		
+		<link rel="stylesheet" href="css/style.css" />
+		<link rel="stylesheet" href="css/custom.css" />
+	</head>
+	<body>
+		<div class="headerlogodiv">
+			<!--
+			<img src="img/headerlogo.png" class="headerlogo"/>
+		-->
+		</div>
+
+
+
+		<nav class="navbar navbar-fixed-top navbar-expand-lg navbar-light">
+			
+	  		<a class="navbar-brand" href="#">Hirev</a>
+	  	
+	  		<button class="navbar-toggler" class="background-color:white" type="button" data-toggle="collapse" data-target="
+	  			#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    		<span class="navbar-toggler-icon"></span>
+	  		</button>
+
+	  		<div class="collapse navbar-collapse text-center" id="navbarSupportedContent">
+			    <ul class="navbar-nav mx-auto">
+			      	<li class="nav-item active  rightborder">
+			        	<a class="nav-link active" href="#">Home</span></a>
+			      	</li>
+
+			      	<li class="nav-item rightborder">
+			        	<a class="nav-link" href="#">About Us</a>
+			      	</li>
+
+			      	<li class="nav-item rightborder">
+			        	<a class="nav-link" href="product.php">Products</a>
+			      	</li>
+
+			      	<li class="nav-item rightborder">
+			        	<a class="nav-link" href="#">Recommender</a>
+			      	</li>
+
+			      	<li class="nav-item rightborder">
+			        	<a class="nav-link" href="#">Contact</a>
+			      	</li>
+
+			      	<li class="nav-item rightborder">
+			        	<a class="nav-link" href="#">Feed</a>
+			      	</li>
+
+			      	<li class="nav-item">
+			        	<a class="nav-link" href="#">Info</a>
+			      	</li>
+			    </ul>			    
+			</div>
+		</nav>
+
+		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+		  <ol class="carousel-indicators">
+		    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+		    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+		    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+		  </ol>
+		  <div class="carousel-inner">
+		    <div class="carousel-item active">
+		      <img class="d-block w-100" src="img/slider.png" alt="First slide">
+		    </div>
+		    <div class="carousel-item">
+		      <img class="d-block w-100" src="img/slider.png" alt="Second slide">
+		    </div>
+		    <div class="carousel-item">
+		      <img class="d-block w-100" src="img/slider.png" alt="Third slide">
+		    </div>
+		  </div>
+		  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+		    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+		    <span class="sr-only">Previous</span>
+		  </a>
+		  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+		    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+		    <span class="sr-only">Next</span>
+		  </a>
+		</div>
+<!--
+		<section class="jumbotron text-center">
+        <div class="container">
+          <h1 class="jumbotron-heading">Album example</h1>
+          <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don't simply skip over it entirely.</p>
+          <p>
+            <a href="#" class="btn btn-primary my-2">Main call to action</a>
+            <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+          </p>
+        </div>
+      </section>
+-->
+	<div class="main py-3">
+      <div class="album py-2 pb-5 mx-5">
+        <div class="container">
+
+          <div class="row ">
+            <div class="col-lg-4 col-md-6 col-sm-12">
+              <div class="card mb-4 shadow-sm">
+                Facebook post
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6  col-sm-12">
+              <div class="card mb-4 shadow-sm">
+                Gallery
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6  col-sm-12">
+              <div class="card mb-4 shadow-sm video-responsive">
+                <iframe width="350" height="210" src="https://www.youtube.com/embed/bXWgheMaUS0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+              </div>
+            </div>
+        </div><!--EndOf main-->
+    </div>
+	</div>
+	</div>
+
+
+	<div class="footer">
+		<div class="bg-vertical-r">
+			About
+		</div>
+		<div class="bg-vertical-r">
+			Latest from us	
+		</div>
+		<div>
+			Contact Us
+		</div>
+	</div>
+
+        <div class="copyright">
+        		COPYRIGHT (C) 2020. www.hi-rev.com.my | TERMS OF USAGE | SUPPORT / HELP
+        </div>
+
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	</body>
+</html>
---
+++ product1.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Revamp</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" 
+			integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  		
+		<link rel="stylesheet" href="css/style.css" />
+		<link rel="stylesheet" href="css/custom.css" />
+	</head>
+	<body>
+		<div class="headerlogodiv">
+			<!--
+			<img src="img/headerlogo.png" class="headerlogo"/>
+		-->
+		</div>
+
+
+
+		<nav class="navbar navbar-fixed-top navbar-expand-lg navbar-light">
+			
+	  		<a class="navbar-brand" href="#">Hirev</a>
+	  	
+	  		<button class="navbar-toggler" class="background-color:white" type="button" data-toggle="collapse" data-target="
+	  			#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+	    		<span class="navbar-toggler-icon"></span>
+	  		</button>
+
+	  		<div class="collapse navbar-collapse text-center" id="navbarSupportedContent">
+			    <ul class="navbar-nav mx-auto">
+			      	<li class="nav-item active  rightborder">
+			        	<a class="nav-link" href="index.php">Home</span></a>
+			      	</li>
+
+			      	<li class="nav-item rightborder">
+			        	<a class="nav-link" href="#">About Us</a>
+			      	</li>
+
+			      	<li class="nav-item rightborder">
+			        	<a class="nav-link active" href="#">Products</a>
+			      	</li>
+
+			      	<li class="nav-item rightborder">
+			        	<a class="nav-link" href="#">Recommender</a>
+			      	</li>
+
+			      	<li class="nav-item rightborder">
+			        	<a class="nav-link" href="#">Contact</a>
+			      	</li>
+
+			      	<li class="nav-item rightborder">
+			        	<a class="nav-link" href="#">Feed</a>
+			      	</li>
+
+			      	<li class="nav-item">
+			        	<a class="nav-link" href="#">Info</a>
+			      	</li>
+			    </ul>			    
+			</div>
+		</nav>
+
+		
+
+	
+        <div class="container white">
+          	<div class="row ">
+            	<div class="col-lg-3 col-md-4 col-sm-12">
+	              	<div class="productmenu mb-4 p-5">
+	              		<h2>Products</h2>
+	                	<a href="#" class="productmenu-link">Motor Oil</a><br/>
+	                	<a href="#" class="productmenu-link">Motorcycle Oil</a><br/>
+	                	<a href="#" class="productmenu-link">Industrial Lubricants</a><br/>
+	                	<a href="#" class="productmenu-link">Other Products</a>
+	              	</div>
+            	</div>
+            	<div class="col-lg-9 col-md-8  col-sm-12">
+              		<div class="productoverview mb-4 pt-3">
+                		<div class="row ">
+			            	<div class="col-md-6 col-sm-12 min-h-350 p-5">
+				              	<a href="#"><img src="img/fulls.png"/></a>
+				              	<h3>Fully Synthetic Motor Oil</h3>
+			            	</div>
+			            	<div class="col-md-6  col-sm-12 min-h-350 p-5">
+			              		<a href="#"><img src="img/semis.png"/></a>
+			              		<h3>Semi-Synthetic Motor Oil</h3>
+			            	</div>            
+			        	</div>
+			        	<div class="row ">
+			            	<div class="col-md-6 col-sm-12 min-h-350 p-5">
+				              	<a href="#"><img src="img/blank.png"/></a>
+				              	<h3>Industrial Lubricants</h3>
+			            	</div>
+			            	<div class="col-md-6  col-sm-12 min-h-230 p-5">
+			              		<a href="#"><img src="img/blank.png"/></a>
+			              		<h3>Other Products</h3>
+			            	</div>            
+			        	</div>
+			        	<div class="row ">
+			            	<div class="col-md-6 col-sm-12 min-h-150 p-5">
+			            		<p class=" assist-link">
+				              		<a href="#"><img src="img/map-pointer.png"/>FIND HI-REV NEAR YOU</a>
+				              	</p>
+			            	</div>
+			            	<div class="col-md-6 col-sm-12 min-h-150 p-5">
+			              		<p class=" assist-link">
+			              			<a href="#">FIND RIGHT OIL FOR YOUR VEHICLE</a>
+			              		</p>
+			            	</div>            
+			        	</div>
+              		</div>
+            	</div>            
+        	</div><!--EndOf row-->
+    	</div>
+	
+
+
+	<div class="footer">
+		<div class="bg-vertical-r">
+			About
+		</div>
+		<div class="bg-vertical-r">
+			Latest from us	
+		</div>
+		<div>
+			Contact Us
+		</div>
+	</div>
+
+        <div class="copyright">
+        		COPYRIGHT (C) 2020. www.hi-rev.com.my | TERMS OF USAGE | SUPPORT / HELP
+        </div>
+
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	</body>
+</html>
